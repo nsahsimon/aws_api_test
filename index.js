552e8b2281a6b6417d6bf9f06ae2b30d00fce7ca
nsahsimon <nsahsimonai01@gmail.com>
2024-05-03 20:05:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+
+app.get("/", (req, res) => {
+    console.log(`we got you`);
+    res.send("we gotto you!")
+})
+
+
+
+// const http = require('http')
+// const port = 8080
+ 
+// // Create a server object:
+// const server = http.createServer(function (req, res) {
+ 
+//     // Write a response to the client
+//     res.write('Hello World')
+ 
+//     // End the response 
+//     res.end()
+// })
+ 
+// // Set up our server so it will listen on the port
+// server.listen(port, function (error) {
+ 
+//     // Checking any error occur while listening on port
+//     if (error) {
+//         console.log('Something went wrong', error);
+//     }
+//     // Else sent message of listening
+//     else {
+//         console.log('Server is listening on port' + port);
+//     }
+// })
