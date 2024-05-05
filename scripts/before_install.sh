94d5321724087e379507040de6478b3492d97078
nsahsimon <nsahsimonai01@gmail.com>
2024-05-05 06:28:27 +0100

    commented everything

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+# #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# #download node and npm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install node
 
-#install pm2
-sudo npm install pm2@latest -g
+# #install pm2
+# sudo npm install pm2@latest -g
 
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  sudo mkdir ${DIR}
-fi
+# #create our working directory if it doesnt exist
+# DIR="/home/ec2-user/express-app"
+# if [ -d "$DIR" ]; then
+#   echo "${DIR} exists"
+# else
+#   echo "Creating ${DIR} directory"
+#   sudo mkdir ${DIR}
+# fi
