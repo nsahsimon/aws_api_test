b3363a203164a7a1ba8b5a057d1d0abd4842dcad
nsahsimon <nsahsimonai01@gmail.com>
2024-05-05 14:45:39 +0100

    fuly restored

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,6 @@
-# # !/bin/bash
-# # stopping existing node servers
-# #sdfddfthtg
-# echo "stopping any existing node servers"
-# # sudo pkill node
+# !/bin/bash
+# stopping existing node servers
+#sdfddfthtg
+echo "stopping any existing node servers"
+sudo pkill node
 # sudo pm2 stop all
